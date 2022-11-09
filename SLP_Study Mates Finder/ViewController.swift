2ade55653b79066a7729e372dd15a8bec8b48362
Ted <taehwan9939@gmail.com>
2022-11-09 23:24:24 +0900

    Initial Commit

---
+++ SLP_Study Mates Finder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SLP_Study Mates Finder
+//
+//  Created by Mac Pro 15 on 2022/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
